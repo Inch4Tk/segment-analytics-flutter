ddaa656fa482f4823197e0e1ac6f572fc45e971d
Tobias Gottwald <tobias.gottwald@gastrobotics.com>
2019-08-31 19:48:36 +0200

    Add more context options
    Also change how the library is loaded and add screen and group as extra
    calls

--- lib/analytics.dart
+++ lib/analytics.dart
@@ -1,4 +1,6 @@
-// Copyright (c) 2017, FaisalAbid. All rights reserved. Use of this source code
+// Original Work Copyright (c) 2017, FaisalAbid.
+// Modified Work Copyright (c) 2019, Tobias Gottwald.
+// All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library SegmentAnalytics;
--- lib/src/analytics_base.dart
+++ lib/src/analytics_base.dart
@@ -1,17 +1,57 @@
+import 'dart:io';
 import 'dart:async';
 import 'dart:convert' show utf8, base64, json, Encoding;
+import 'package:connectivity/connectivity.dart';
 import 'package:http/http.dart';
-
+import 'package:device_info/device_info.dart';
 
 class Analytics {
+  static bool enabled = true;
   static Analytics _singleton;
   static String endpoint = "https://api.segment.io/v1";
   static String writeKey;
   static AnalyticsClient client = new AnalyticsClient(new Client());
 
-  factory Analytics({String apiKey}) {
+  // Context infos
+  static String osName;
+  static String osVersion;
+  static String deviceId;
+  static String deviceManufacturer;
+  static String deviceModel;
+
+  static String appName;
+  static String appVersion;
+  static String appBuild;
+  static String screenWidth;
+  static String screenHeight;
+  static String locale;
+
+  static Future<void> _loading;
+
+  factory Analytics.load(
+      {String apiKey,
+      String appName,
+      String appVersion,
+      String appBuild,
+      String screenWidth,
+      String screenHeight,
+      String locale}) {
+    Analytics.appName = appName;
+    Analytics.appVersion = appVersion;
+    Analytics.appBuild = appBuild;
+    Analytics.screenWidth = screenWidth;
+    Analytics.screenHeight = screenHeight;
+    Analytics.locale = locale;
+    _singleton = new Analytics._internal(apiKey);
+    return _singleton;
+  }
+
+  factory Analytics() {
     if (_singleton == null) {
-      _singleton = new Analytics._internal(apiKey);
+      if (enabled) {
+        print("Warning: Called Analytics without loading the library.");
+      }
+      _singleton = new Analytics._internalNoWriteKey();
       return _singleton;
     } else {
       return _singleton;
@@ -19,36 +59,99 @@ class Analytics {
   }
 
   Analytics._internal(String segmentApiKey) {
-    writeKey = "Basic ${"$base64.encode(utf8.encode(segmentApiKey)).toString()"}";
+    writeKey = "Basic ${base64.encode(utf8.encode(segmentApiKey)).toString()}";
+    Analytics._loading = _loadDeviceInfos();
   }
 
-  void identify(String userID, Map properties) {
-    Map payload = {"type": "identify", "traits": properties, "userId": userID, "context": defaultContext()};
-    client.postSilentMicrotask("$endpoint/identify", body: json.encode(payload));
+  Analytics._internalNoWriteKey();
+
+  Future<void> _loadDeviceInfos() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      osName = androidInfo.version.baseOS;
+      osVersion = androidInfo.version.release;
+      deviceId = androidInfo.androidId;
+      deviceManufacturer = androidInfo.manufacturer;
+      deviceModel = androidInfo.model;
+    } else {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      osName = iosInfo.systemName;
+      osVersion = iosInfo.systemVersion;
+      deviceId = iosInfo.identifierForVendor;
+      deviceManufacturer = "Apple";
+      deviceModel = iosInfo.model;
+    }
   }
 
-  void identifyIP(String userID, String ip) {
-    Map context = defaultContext();
-    context["ip"] = ip;
-    Map payload = {"type": "identify", "userId": userID, "context": context};
-    client.postSilentMicrotask("$endpoint/identify", body: json.encode(payload));
+  void identify(String userID, {Map traits}) async {
+    if (!enabled) {
+      return;
+    }
+    if (traits == null) {
+      traits = new Map();
+    }
+
+    Map payload = {
+      "traits": traits,
+      "userId": userID,
+      "context": await defaultContext()
+    };
+    client.postSilentMicrotask("$endpoint/identify",
+        body: json.encode(payload));
   }
 
-  trackRevenue(String userId, double value, String eventName) async {
-    Map properties = {"revenue": value};
-    track(userId, eventName, properties: properties);
+  void group(String userID, String groupId, {Map traits}) async {
+    if (!enabled) {
+      return;
+    }
+    if (traits == null) {
+      traits = new Map();
+    }
+
+    Map payload = {
+      "traits": traits,
+      "userId": userID,
+      "groupId": groupId,
+      "context": await defaultContext()
+    };
+    client.postSilentMicrotask("$endpoint/group", body: json.encode(payload));
   }
 
-
-  void track(String userId, String event, {Map properties, Map context}) {
+  Future<void> screen(String userId, String name,
+      {Map properties, Map context}) async {
+    if (!enabled) {
+      return;
+    }
     if (properties == null) {
       properties = new Map();
     }
 
-    if (context == null) {
-      context = defaultContext();
-    } else {
-      context.addAll(defaultContext());
+    Map sendContext = await defaultContext();
+    if (context != null) {
+      sendContext.addAll(context);
+    }
+
+    Map payload = {
+      "userId": userId,
+      "context": new Map.from(context),
+      "name": name,
+      "properties": new Map.from(properties)
+    };
+    client.postSilentMicrotask("$endpoint/screen", body: json.encode(payload));
+  }
+
+  void track(String userId, String event, {Map properties, Map context}) async {
+    if (!enabled) {
+      return;
+    }
+    if (properties == null) {
+      properties = new Map();
+    }
+
+    Map sendContext = await defaultContext();
+    if (context != null) {
+      sendContext.addAll(context);
     }
 
     Map payload = {
@@ -60,9 +163,32 @@ class Analytics {
     client.postSilentMicrotask("$endpoint/track", body: json.encode(payload));
   }
 
-  Map defaultContext() {
+  Future<Map> defaultContext() async {
+    // Check connectivity
+    final connectivity = await (Connectivity().checkConnectivity());
+    bool mobile = false;
+    bool wifi = false;
+    if (connectivity == ConnectivityResult.mobile) {
+      mobile = true;
+    }
+    if (connectivity == ConnectivityResult.wifi) {
+      wifi = true;
+    }
+    final String timezone = DateTime.now().timeZoneName;
     return {
-      "library": {"name": "analytics-dart", "version": "0.1"}
+      "library": {"name": "analytics-flutter", "version": "1.0.0"},
+      "app": {"name": appName, "version": appVersion, "build": appBuild},
+      "device": {
+        "id": deviceId,
+        "manufacturer": deviceManufacturer,
+        "model": deviceModel
+      },
+      "network": {"cellular": mobile, "wifi": wifi},
+      "screen": {"height": screenHeight, "width": screenWidth},
+      "os": {"name": osName, "version": osVersion},
+      "ip": "0.0.0.0",
+      "timezone": timezone,
+      "locale": locale
     };
   }
 }
@@ -76,11 +202,18 @@ class AnalyticsClient extends BaseClient {
   Future<StreamedResponse> send(BaseRequest request) {
     request.headers['Content-Type'] = "application/json";
     request.headers["Authorization"] = Analytics.writeKey;
-    print(request.headers);
     return _inner.send(request);
   }
 
-  postSilentMicrotask(url, {Map<String, String> headers, body, Encoding encoding}) async {
+  postSilentMicrotask(url,
+      {Map<String, String> headers, body, Encoding encoding}) async {
+    if (Analytics.writeKey == null) {
+      print("Warning: Library was not loaded or no write key was supplied.");
+      return;
+    }
+    // Make sure everything is fully loaded;
+    await Analytics._loading;
+
     scheduleMicrotask(() async {
       await this.post(url, headers: headers, body: body, encoding: encoding);
     });
